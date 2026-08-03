79004f382717e6fa2a5290ec0c0c710de03a0562
DRM-236 <drm04@proton.me>
2026-08-03 19:59:19 +0200

    Ahora funciona en java y python al menos en mi maquina

--- Java/gestion_partituras/LectorMusica.java
+++ Java/gestion_partituras/LectorMusica.java
@@ -4,11 +4,14 @@ import java.io.InputStreamReader;
 
 public class LectorMusica {
 	
-	private final String RUTA_SCRIPT_LECTORNOTAS_PY = "/tfg_java/Python/LectorNotas.py";
+	private final String RUTA_SCRIPT_LECTORNOTAS_PY = "Aplicacion-ia-musical/Python/LectorNotas.py";
+    private final String RUTA_INTERPRETE = ".digitador/bin/python3";
 	
     public void leerMidXml(String rutaPartitura) {
         try {
-            ProcessBuilder pb = new ProcessBuilder("python3", RUTA_SCRIPT_LECTORNOTAS_PY, rutaPartitura); // Ejecutar python, archivo python (sacador de notas), midi/xml a ver
+            ProcessBuilder pb = new ProcessBuilder(RUTA_INTERPRETE, RUTA_SCRIPT_LECTORNOTAS_PY, rutaPartitura); // Ejecutar python, archivo python (sacador de notas), midi/xml a ver
+
+            pb.redirectErrorStream(true);
 
             Process process = pb.start();
 
@@ -16,8 +19,11 @@ public class LectorMusica {
             
             String jsonOutput = reader.readLine(); // JSON con la info de las notas/acordes
 
-            System.out.println("JSON listo en Java: " + jsonOutput);
+            //while((jsonOutput = reader.readLine()) != null) {
+            //    System.out.println("Salida de python: " + jsonOutput);
+            //}
 
+            System.out.println("JSON listo en Java: " + jsonOutput);
             // Parsear JSON con la librería Jackson si se necesita
 
         } catch (Exception e) {
