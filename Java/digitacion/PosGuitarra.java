054e0d5d58e299c1000692da3b92474b823f79f7
DRM-236 <drm04@proton.me>
2026-08-08 14:48:35 +0200

    Se ha terminado una primera version de digitacion y creacion de JSON para comunicar a python. Se ha creado la clase de PartituraDigitada(vacia de momento) Se ha cambiado el nombre de LectorMusica a EmbajadorMusic21Python para hacer de ella una clase que sirva para todas las comunicaciones entre java y python. Se ha creado la clase PosGuitarra para guardar la informacion de cuerda y traste de la digitacion. Se considera que no hay octavas(es decir, que todos los do, rem etc. son el mismo) en esta primera version de la digitacion.

---
+++ Java/digitacion/PosGuitarra.java
@@ -0,0 +1,21 @@
+package digitacion;
+
+public class PosGuitarra {
+	private int cuerda;
+	private int traste;
+	
+	public PosGuitarra(int cu, int tr) {
+		cuerda = cu;
+		traste = tr;
+	}
+
+	public int getCuerda() {
+		return cuerda;
+	}
+
+	public int getTraste() {
+		return traste;
+	}
+	
+	
+}
---
+++ Java/gestion_partituras/PartituraDigitada.java
@@ -0,0 +1,9 @@
+package gestion_partituras;
+
+public class PartituraDigitada extends Partitura {
+
+	public PartituraDigitada(String nombre) {
+		super(nombre);
+	}
+
+}
